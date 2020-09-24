f3dca184724d30221685ed36a3efefa22a319ef2
lmsilva-oss <62002634+lmsilva-oss@users.noreply.github.com>
2020-09-24 01:34:04 -0300

    Create basic integration test for creating a new Address

--- src/test/java/com/lmsilva/geocoding/GeocodingApplicationTests.java
+++ src/test/java/com/lmsilva/geocoding/GeocodingApplicationTests.java
@@ -1,13 +1,53 @@
 package com.lmsilva.geocoding;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lmsilva.geocoding.data.Address;
+import com.lmsilva.geocoding.repository.AddressRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GeocodingApplicationTests {
+	@MockBean
+	private AddressRepository addressRepository;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	private static final ObjectMapper MAPPER = new ObjectMapper();
 
 	@Test
-	void contextLoads() {
+	void testCreateAddress() throws Exception {
+		Address address = new Address();
+		address.setId("testId");
+		address.setStreetName("testStreetName");
+		address.setNumber("testNumber");
+		address.setNeighborhood("testNeighborhood");
+		address.setCity("testCity");
+		address.setState("testState");
+		address.setCountry("testCountry");
+		address.setZipcode("testZipcode");
+
+		String expectedResponse = MAPPER.writeValueAsString(address);
+
+		given(addressRepository.createAddress(any(Address.class))).willReturn(address);
+
+		mockMvc.perform(post("/address")
+				.contentType("application/json")
+				.content(expectedResponse))
+			.andExpect(status().isOk())
+			.andExpect(content().string(expectedResponse));
 	}
 
 }
