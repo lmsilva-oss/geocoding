5350dcdf639d47ea973bb1ccb575443b2683bdd6
lmsilva-oss <62002634+lmsilva-oss@users.noreply.github.com>
2020-09-24 02:01:01 -0300

    Re-add @JsonIgnore on NON_NULL fields for Address

--- src/main/java/com/lmsilva/geocoding/data/Address.java
+++ src/main/java/com/lmsilva/geocoding/data/Address.java
@@ -1,10 +1,11 @@
 package com.lmsilva.geocoding.data;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lmsilva.geocoding.exception.MissingRequiredParameterException;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Address {
     private String id;
     private String streetName;
